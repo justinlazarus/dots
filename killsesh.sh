92381103be7eef04e5aacc9a0d8c5d46bf9d6200
Justin Lazarus <jlazarus@costco.com>
2024-10-05 14:35:02 -0700

    many things

---
+++ killsesh.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+worktree_dir="$HOME/intl-depot.git/worktrees"
+bare_repo_dir="$HOME/intl-depot.git"
+
+if [ ! -d "$worktree_dir" ]; then
+    echo "Directory $worktree_dir does not exist."
+    exit 1
+fi
+
+sessions=$(tmux list-sessions -F "#{session_name}" 2>/dev/null)
+
+if [ -z "$sessions" ]; then
+    echo "No tmux sessions found."
+    exit 0
+fi
+
+for subdir in "$worktree_dir"/*/; do
+    if [ -d "$subdir" ]; then
+        session_name=$(basename "$subdir")
+
+        if tmux has-session -t "$session_name" 2>/dev/null; then
+            tmux kill-session -t "$session_name"
+            echo "Killed tmux session $session_name."
+        else
+            echo "Session $session_name does not exist."
+        fi
+    fi
+done
+
+echo "All matching sesssions have been killed."
---
+++ reptosesh.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+worktree_dir="$HOME/intl-depot.git/worktrees"
+bare_repo_dir="$HOME/intl-depot.git"
+
+if [ ! -d "$worktree_dir" ]; then
+    echo "Directory $worktree_dir does not exist."
+    exit 1
+fi
+
+for subdir in "$worktree_dir"/*/; do
+    if [ -d "$subdir" ]; then
+        session_name=$(basename "$subdir")
+        session_dir="$bare_repo_dir/$session_name/intl-depot/"
+
+        # Check if a session with the same name already exists
+        tmux has-session -t "$session_name" 2>/dev/null
+
+        if [ $? -ne 0 ]; then
+
+            # Create a new session and the first window for nvim
+            tmux new-session -d -s "$session_name" -c "$session_dir" -n "nvim"
+
+            # Create additional windows for nx and git
+            tmux new-window -t "$session_name" -n "nx" -c "$session_dir"
+            tmux new-window -t "$session_name" -n "git" -c "$session_dir"
+
+            # select the first window
+            tmux select-window -t "$session_name":0
+
+            echo "Created new tmux session: $session_name with 3 windows"
+        else
+            echo "Session $session_name already exists"
+        fi
+    fi
+done
