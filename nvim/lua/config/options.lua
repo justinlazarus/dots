061bc6178662fbac10a9ccc31d1d61e5f3833462
Justin Lazarus <jlazarus@costco.com>
2025-07-18 15:53:49 -0700

    added arch folder and started adding configs

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -5,7 +5,7 @@ vim.o.number = true
 vim.o.mouse = 'a'
 vim.o.showmode = false
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus'
+  vim.o.clipboard='unnamedplus'
 end)
 vim.o.breakindent = true
 vim.o.undofile = true
