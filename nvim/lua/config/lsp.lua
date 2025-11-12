95eaf537723d69290113fc5b293effbaade1438d
Justin Lazarus [jlazarus] <jlazarus@costco.com>
2025-11-11 22:55:30 -0800

    miscellany

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,29 +1,11 @@
 -- Suppress deprecation warnings from Neovim dev builds
 vim.deprecate = function() end
 
--- Manually start LSP servers on FileType
--- (Not using vim.lsp.enable to avoid conflicts with roslyn.nvim's custom setup)
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = 'lua',
-  callback = function(args)
-    vim.lsp.start({
-      name = 'lua_ls',
-      cmd = { vim.fn.stdpath('data') .. '/mason/bin/lua-language-server' },
-      root_dir = vim.fs.root(args.buf, {'.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git'}),
-    })
-  end,
-})
-
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = {'javascript', 'javascriptreact', 'typescript', 'typescriptreact'},
-  callback = function(args)
-    vim.lsp.start({
-      name = 'ts_ls',
-      cmd = { vim.fn.stdpath('data') .. '/mason/bin/typescript-language-server', '--stdio' },
-      root_dir = vim.fs.root(args.buf, {'package.json', 'tsconfig.json', 'jsconfig.json', '.git'}),
-    })
-  end,
-})
+-- Enable LSP servers (roslyn is handled separately in plugin.lua)
+vim.lsp.enable {
+  'lua_ls',
+  'ts_ls',
+}
 
 -- When an LSP attaches, set buffer-local keymaps for code actions
 vim.api.nvim_create_autocmd('LspAttach', {
--- nvim/lua/config/plugin.lua
+++ nvim/lua/config/plugin.lua
@@ -104,170 +104,70 @@ Snacks.setup {
   words = {},
 }
 
--- Snacks keymaps converted from the plugin spec keys table
+-- Snacks keymaps
 local map = vim.keymap.set
 
 -- Top Pickers & Explorer
-map('n', '<leader><space>', function()
-  Snacks.picker.smart()
-end, { desc = 'Smart Find Files' })
-map('n', '<leader>,', function()
-  Snacks.picker.buffers()
-end, { desc = 'Buffers' })
-map('n', '<leader>/', function()
-  Snacks.picker.grep()
-end, { desc = 'Grep' })
-map('n', '<leader>:', function()
-  Snacks.picker.command_history()
-end, { desc = 'Command History' })
-map('n', '<leader>n', function()
-  Snacks.picker.notifications()
-end, { desc = 'Notification History' })
-map('n', '<leader>e', function()
-  Snacks.explorer()
-end, { desc = 'File Explorer' })
+map('n', '<leader><space>', function() Snacks.picker.smart() end, { desc = 'Smart Find Files' })
+map('n', '<leader>,', function() Snacks.picker.buffers() end, { desc = 'Buffers' })
+map('n', '<leader>/', function() Snacks.picker.grep() end, { desc = 'Grep' })
+map('n', '<leader>:', function() Snacks.picker.command_history() end, { desc = 'Command History' })
+map('n', '<leader>n', function() Snacks.picker.notifications() end, { desc = 'Notification History' })
+map('n', '<leader>e', function() Snacks.explorer() end, { desc = 'File Explorer' })
 
 -- find
-map('n', '<leader>fb', function()
-  Snacks.picker.buffers()
-end, { desc = 'Buffers' })
-map('n', '<leader>fc', function()
-  Snacks.picker.files { cwd = vim.fn.stdpath 'config' }
-end, { desc = 'Find Config File' })
-map('n', '<leader>ff', function()
-  Snacks.picker.files()
-end, { desc = 'Find Files' })
-map('n', '<leader>fg', function()
-  Snacks.picker.git_files()
-end, { desc = 'Find Git Files' })
-map('n', '<leader>fp', function()
-  Snacks.picker.projects()
-end, { desc = 'Projects' })
-map('n', '<leader>fr', function()
-  Snacks.picker.recent()
-end, { desc = 'Recent' })
+map('n', '<leader>fb', function() Snacks.picker.buffers() end, { desc = 'Buffers' })
+map('n', '<leader>fc', function() Snacks.picker.files { cwd = vim.fn.stdpath 'config' } end, { desc = 'Find Config File' })
+map('n', '<leader>ff', function() Snacks.picker.files() end, { desc = 'Find Files' })
+map('n', '<leader>fg', function() Snacks.picker.git_files() end, { desc = 'Find Git Files' })
+map('n', '<leader>fp', function() Snacks.picker.projects() end, { desc = 'Projects' })
+map('n', '<leader>fr', function() Snacks.picker.recent() end, { desc = 'Recent' })
 
 -- git
-map('n', '<leader>gb', function()
-  Snacks.picker.git_branches()
-end, { desc = 'Git Branches' })
-map('n', '<leader>gl', function()
-  Snacks.picker.git_log()
-end, { desc = 'Git Log' })
-map('n', '<leader>gL', function()
-  Snacks.picker.git_log_line()
-end, { desc = 'Git Log Line' })
-map('n', '<leader>gs', function()
-  Snacks.picker.git_status()
-end, { desc = 'Git Status' })
-map('n', '<leader>gS', function()
-  Snacks.picker.git_stash()
-end, { desc = 'Git Stash' })
-map('n', '<leader>gd', function()
-  Snacks.picker.git_diff()
-end, { desc = 'Git Diff (Hunks)' })
-map('n', '<leader>gf', function()
-  Snacks.picker.git_log_file()
-end, { desc = 'Git Log File' })
+map('n', '<leader>gb', function() Snacks.picker.git_branches() end, { desc = 'Git Branches' })
+map('n', '<leader>gl', function() Snacks.picker.git_log() end, { desc = 'Git Log' })
+map('n', '<leader>gL', function() Snacks.picker.git_log_line() end, { desc = 'Git Log Line' })
+map('n', '<leader>gs', function() Snacks.picker.git_status() end, { desc = 'Git Status' })
+map('n', '<leader>gS', function() Snacks.picker.git_stash() end, { desc = 'Git Stash' })
+map('n', '<leader>gd', function() Snacks.picker.git_diff() end, { desc = 'Git Diff (Hunks)' })
+map('n', '<leader>gf', function() Snacks.picker.git_log_file() end, { desc = 'Git Log File' })
 
 -- Grep / buffer lines
-map('n', '<leader>sb', function()
-  Snacks.picker.lines()
-end, { desc = 'Buffer Lines' })
-map('n', '<leader>sB', function()
-  Snacks.picker.grep_buffers()
-end, { desc = 'Grep Open Buffers' })
-map('n', '<leader>sg', function()
-  Snacks.picker.grep()
-end, { desc = 'Grep' })
-map({ 'n', 'x' }, '<leader>sw', function()
-  Snacks.picker.grep_word()
-end, { desc = 'Visual selection or word' })
+map('n', '<leader>sb', function() Snacks.picker.lines() end, { desc = 'Buffer Lines' })
+map('n', '<leader>sB', function() Snacks.picker.grep_buffers() end, { desc = 'Grep Open Buffers' })
+map('n', '<leader>sg', function() Snacks.picker.grep() end, { desc = 'Grep' })
+map({ 'n', 'x' }, '<leader>sw', function() Snacks.picker.grep_word() end, { desc = 'Visual selection or word' })
 
 -- search helpers
-map('n', '<leader>s"', function()
-  Snacks.picker.registers()
-end, { desc = 'Registers' })
-map('n', '<leader>s/', function()
-  Snacks.picker.search_history()
-end, { desc = 'Search History' })
-map('n', '<leader>sa', function()
-  Snacks.picker.autocmds()
-end, { desc = 'Autocmds' })
-map('n', '<leader>sc', function()
-  Snacks.picker.command_history()
-end, { desc = 'Command History' })
-map('n', '<leader>sC', function()
-  Snacks.picker.commands()
-end, { desc = 'Commands' })
-map('n', '<leader>sd', function()
-  Snacks.picker.diagnostics()
-end, { desc = 'Diagnostics' })
-map('n', '<leader>sD', function()
-  Snacks.picker.diagnostics_buffer()
-end, { desc = 'Buffer Diagnostics' })
-map('n', '<leader>sh', function()
-  Snacks.picker.help()
-end, { desc = 'Help Pages' })
-map('n', '<leader>sH', function()
-  Snacks.picker.highlights()
-end, { desc = 'Highlights' })
-map('n', '<leader>si', function()
-  Snacks.picker.icons()
-end, { desc = 'Icons' })
-map('n', '<leader>sj', function()
-  Snacks.picker.jumps()
-end, { desc = 'Jumps' })
-map('n', '<leader>sk', function()
-  Snacks.picker.keymaps()
-end, { desc = 'Keymaps' })
-map('n', '<leader>sl', function()
-  Snacks.picker.loclist()
-end, { desc = 'Location List' })
-map('n', '<leader>sm', function()
-  Snacks.picker.marks()
-end, { desc = 'Marks' })
-map('n', '<leader>sM', function()
-  Snacks.picker.man()
-end, { desc = 'Man Pages' })
-map('n', '<leader>sp', function()
-  Snacks.picker.lazy()
-end, { desc = 'Search for Plugin Spec' })
-map('n', '<leader>sq', function()
-  Snacks.picker.qflist()
-end, { desc = 'Quickfix List' })
-map('n', '<leader>sR', function()
-  Snacks.picker.resume()
-end, { desc = 'Resume' })
-map('n', '<leader>su', function()
-  Snacks.picker.undo()
-end, { desc = 'Undo History' })
-map('n', '<leader>uC', function()
-  Snacks.picker.colorschemes()
-end, { desc = 'Colorschemes' })
+map('n', '<leader>s"', function() Snacks.picker.registers() end, { desc = 'Registers' })
+map('n', '<leader>s/', function() Snacks.picker.search_history() end, { desc = 'Search History' })
+map('n', '<leader>sa', function() Snacks.picker.autocmds() end, { desc = 'Autocmds' })
+map('n', '<leader>sc', function() Snacks.picker.command_history() end, { desc = 'Command History' })
+map('n', '<leader>sC', function() Snacks.picker.commands() end, { desc = 'Commands' })
+map('n', '<leader>sd', function() Snacks.picker.diagnostics() end, { desc = 'Diagnostics' })
+map('n', '<leader>sD', function() Snacks.picker.diagnostics_buffer() end, { desc = 'Buffer Diagnostics' })
+map('n', '<leader>sh', function() Snacks.picker.help() end, { desc = 'Help Pages' })
+map('n', '<leader>sH', function() Snacks.picker.highlights() end, { desc = 'Highlights' })
+map('n', '<leader>si', function() Snacks.picker.icons() end, { desc = 'Icons' })
+map('n', '<leader>sj', function() Snacks.picker.jumps() end, { desc = 'Jumps' })
+map('n', '<leader>sk', function() Snacks.picker.keymaps() end, { desc = 'Keymaps' })
+map('n', '<leader>sl', function() Snacks.picker.loclist() end, { desc = 'Location List' })
+map('n', '<leader>sm', function() Snacks.picker.marks() end, { desc = 'Marks' })
+map('n', '<leader>sM', function() Snacks.picker.man() end, { desc = 'Man Pages' })
+map('n', '<leader>sp', function() Snacks.picker.lazy() end, { desc = 'Search for Plugin Spec' })
+map('n', '<leader>sq', function() Snacks.picker.qflist() end, { desc = 'Quickfix List' })
+map('n', '<leader>sR', function() Snacks.picker.resume() end, { desc = 'Resume' })
+map('n', '<leader>su', function() Snacks.picker.undo() end, { desc = 'Undo History' })
+map('n', '<leader>uC', function() Snacks.picker.colorschemes() end, { desc = 'Colorschemes' })
 
 -- LSP-related pickers
-map('n', 'gd', function()
-  Snacks.picker.lsp_definitions()
-end, { desc = 'Goto Definition' })
-map('n', 'gD', function()
-  Snacks.picker.lsp_declarations()
-end, { desc = 'Goto Declaration' })
-map('n', 'gr', function()
-  Snacks.picker.lsp_references()
-end, { nowait = true, desc = 'References' })
-map('n', 'gI', function()
-  Snacks.picker.lsp_implementations()
-end, { desc = 'Goto Implementation' })
-map('n', 'gy', function()
-  Snacks.picker.lsp_type_definitions()
-end, { desc = 'Goto Type Definition' })
-map('n', '<leader>ss', function()
-  Snacks.picker.lsp_symbols()
-end, { desc = 'LSP Symbols' })
-map('n', '<leader>sS', function()
-  Snacks.picker.lsp_workspace_symbols()
-end, { desc = 'LSP Workspace Symbols' })
+map('n', 'gd', function() Snacks.picker.lsp_definitions() end, { desc = 'Goto Definition' })
+map('n', 'gD', function() Snacks.picker.lsp_declarations() end, { desc = 'Goto Declaration' })
+map('n', 'gr', function() Snacks.picker.lsp_references() end, { nowait = true, desc = 'References' })
+map('n', 'gI', function() Snacks.picker.lsp_implementations() end, { desc = 'Goto Implementation' })
+map('n', 'gy', function() Snacks.picker.lsp_type_definitions() end, { desc = 'Goto Type Definition' })
+map('n', '<leader>ss', function() Snacks.picker.lsp_symbols() end, { desc = 'LSP Symbols' })
+map('n', '<leader>sS', function() Snacks.picker.lsp_workspace_symbols() end, { desc = 'LSP Workspace Symbols' })
 
 -----------------------------------------------------------------------------------------------------which-key
 
@@ -312,18 +212,35 @@ require('blink.cmp').setup {
 require('conform').setup {
   notify_on_error = false,
   format_on_save = function(bufnr)
+    -- Check if format-on-save is disabled globally or for this buffer
+    if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+      return nil
+    end
+
+    -- Skip formatting for specific filetypes
     local disable_filetypes = { c = true, cpp = true }
     if disable_filetypes[vim.bo[bufnr].filetype] then
       return nil
-    else
-      return { timeout_ms = 500, lsp_format = 'fallback' }
     end
+
+    return { timeout_ms = 500, lsp_format = 'fallback' }
   end,
   formatters_by_ft = { lua = { 'stylua' } },
 }
+
 vim.keymap.set('n', '<leader>f', function()
   require('conform').format { async = true, lsp_format = 'fallback' }
-end, { desc = '[F]ormat buffer' })
+end, { desc = 'Format buffer' })
+
+-- Toggle format-on-save
+vim.keymap.set('n', '<leader>tf', function()
+  vim.g.disable_autoformat = not vim.g.disable_autoformat
+  if vim.g.disable_autoformat then
+    vim.notify('Format-on-save disabled', vim.log.levels.INFO)
+  else
+    vim.notify('Format-on-save enabled', vim.log.levels.INFO)
+  end
+end, { desc = 'Toggle format-on-save' })
 
 ----------------------------------------------------------------------------------------------------- gitsigns
 
