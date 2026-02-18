598df8f9f0716eafb65da19306c0a3178fdbc89c
Justin Lazarus [jlazarus] <jlazarus@costco.com>
2026-02-18 00:28:25 -0800

    more angular

--- nvim/lsp/angularls.lua
+++ nvim/lsp/angularls.lua
@@ -1,12 +1,16 @@
 local mason_path = vim.fn.stdpath('data') .. '/mason/packages/angular-language-server/node_modules'
 
 return {
-  cmd = {
-    'ngserver',
-    '--stdio',
-    '--tsProbeLocations', mason_path,
-    '--ngProbeLocations', mason_path,
-  },
+  cmd = function(dispatchers)
+    local root = vim.fs.root(0, 'angular.json') or vim.fn.getcwd()
+    local probe = mason_path .. ',' .. root .. '/node_modules'
+    return vim.lsp.rpc.start({
+      'ngserver',
+      '--stdio',
+      '--tsProbeLocations', probe,
+      '--ngProbeLocations', probe,
+    }, dispatchers)
+  end,
   filetypes = { 'typescript', 'html', 'htmlangular' },
   root_markers = { 'angular.json' },
 }
